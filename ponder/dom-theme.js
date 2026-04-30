04c360b613f1d8036c98b8248de665a8d7fb0368
Sparky <5hq8r7k4dk@privaterelay.appleid.com>
2026-04-29 19:59:25 -0600

    Completed dome-theme assignment by adding Circle of Time theme

--- ponder/dom-theme.js
+++ ponder/dom-theme.js
@@ -1,24 +1,29 @@
 
 let selectElem = document.querySelector('#theme-select');
 let pageContent = document.querySelector('body');
+let logo = document.getElementById("logo");
 
 selectElem.addEventListener('change', changeTheme);
 
 function changeTheme() {
     let current = selectElem.value;
     if (current === 'ocean') {
-        document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/ocean.jpg')";
+        document.body.style.backgroundImage = "url(images/forevernow.jpg)";
         pageContent.style.fontFamily = "Papyrus, fantasy";
+        logo.style.opacity = "100%";
     } else if (current === 'forest') {
-        document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/forest.jpg')";
+        document.body.style.backgroundImage = "url(images/somethingeverything.jpg)";
         pageContent.style.fontFamily = "Impact, sans-serif";
+        logo.style.opacity = "100%";
     } else if (current === 'desert') {
-        document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/desert.jpg')";
+        document.body.style.backgroundImage = "url(images/endlessdawn.jpg)";
         pageContent.style.fontFamily = "'Big Caslon', serif";
+        logo.style.opacity = "100%";
     } else {
         // default
-        document.body.style.backgroundImage = "none";
+        document.body.style.backgroundImage = "url(images/circleintime.jpg)";
         pageContent.style.fontFamily = "Georgia, serif";
+        logo.style.opacity = "0%";
     }
 }
           
